91740de457c5d47aaf915b05cb25946f3611a1a0
Dominik Kondrot <dominik.kondrot@gmail.com>
2020-12-16 22:50:12 +0100

    GroupController - Wstępna implementacja endpointów
    Wstępna implementacja GroupController
    Utworzenie GroupDto

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/v1/group")
 public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<GroupDto> getGroups() {
+        GroupDto[] groups = new GroupDto[]{
+                new GroupDto(1L,"Test Group 1"),
+                new GroupDto(2L,"Test Group 2"),
+                new GroupDto(3L,"Test Group 3")
+        };
+        return Arrays.asList(groups);
+    }
+
+    @GetMapping(value = "getGroup")
+    public GroupDto getGroup(@RequestParam Long id) {
+        return new GroupDto(5L,"getGroup Test");
+    }
+
+    @PostMapping(value = "addGroup")
+    public GroupDto addGroup(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
+
+    @PutMapping(value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -1,4 +1,11 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class GroupDto {
+    private final Long groupId;
+    private final String groupName;
 }
