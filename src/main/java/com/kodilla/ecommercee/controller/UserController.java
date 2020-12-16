6f2d57b7b5b5dc0e2872521a35b6687238536727
tosuch <tomasz.osuch@gmail.com>
2020-12-17 00:07:00 +0100

    JDP201202-5 - wstępna implementacja endpointw oraz dodanie klasy UserDto

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.UserDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
 public class UserController {
+
+    @GetMapping("/getUser/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return new UserDto(id, "test", "1", 1234L);
+    }
+
+    @PutMapping(value = "updateUser")
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        return userDto;
+    }
+
+    @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return userDto;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/UserDto.java
@@ -1,4 +1,14 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserDto {
+
+    private Long id;
+    private String username;
+    private String status;
+    private Long userKey;
 }
