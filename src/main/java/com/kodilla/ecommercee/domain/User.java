4548a197a7a564c4babe3f579b3eaf5844db811d
tosuch <tomasz.osuch@gmail.com>
2020-12-17 19:06:46 +0100

    task-JDP201202-15 Implementacja Encji User

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @NotNull
+    @Column(name = "status")
+    private String status;
+    @Column(name = "userKey")
+    private Long userKey;
+/*    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orders;*/
 }
