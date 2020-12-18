01e77d5bfca989d11224c3293e5ffadef64f516b
Adam Tomys <adamtomys@gmail.com>
2020-12-18 10:12:22 +0100

    Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,5 +1,29 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Value
+@NoArgsConstructor
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "PRODUCTS")
 public class Product {
+
+    @Id
+    Long id;
+    String name;
+    String description;
+    BigDecimal price;
+    int groupId;
+
+
 
 }
