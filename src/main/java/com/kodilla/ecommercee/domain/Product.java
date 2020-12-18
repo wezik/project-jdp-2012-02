ae1db62db5a9400ab727b80a81d6ff03f21c8ae2
Adam Tomys <adamtomys@gmail.com>
2020-12-18 10:44:38 +0100

    Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,29 +1,41 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.Value;
+import com.sun.istack.internal.NotNull;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
-@Value
+@Data
 @NoArgsConstructor
-@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity(name = "PRODUCTS")
 public class Product {
 
     @Id
+    @GeneratedValue
+    @Column(name = "PRODUCT_ID", unique = true)
     Long id;
+    @NotNull
+    @Column(name = "NAME")
     String name;
+    @NotNull
+    @Column(name = "DESCRIPTION")
     String description;
+    @NotNull
+    @Column(name = "PRICE")
     BigDecimal price;
-    int groupId;
-
-
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    Group groupId;
+    @ManyToMany
+    @JoinTable(
+            name = "PRODUCTS_IN_CARTS",
+            joinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")}
+    )
+    List<Cart> cartsWhichContainsThisProduct;
 
 }
