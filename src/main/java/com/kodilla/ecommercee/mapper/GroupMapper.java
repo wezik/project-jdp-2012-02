a5abd8e5ba959a5812a45b5c8bbf5fb0182270a8
Dominik Kondrot <dominik.kondrot@gmail.com>
2020-12-18 15:19:48 +0100

    GroupController - rzeczywista implementacja
    - Implementacja Metod w GroupController
    - GroupDbService
    - GroupMapper
    - GroupNotFoundException
    - GroupRepository
    
    Z racji zmian w repo napisałem testy sprawdzające
    - GroupRepositoryTest

--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -1,4 +1,25 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class GroupMapper {
+    public Group mapToGroup(GroupDto groupDto) {
+        return new Group(groupDto.getGroupId(),groupDto.getGroupName());
+    }
+
+    public GroupDto mapToGroupDto(Group group) {
+        return new GroupDto(group.getId(),group.getGroupName());
+    }
+
+    public List<GroupDto> mapToGroupDtoList(List<Group> groups) {
+        return groups.stream()
+                .map(this::mapToGroupDto)
+                .collect(Collectors.toList());
+    }
 }
