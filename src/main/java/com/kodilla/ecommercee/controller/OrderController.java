18bef838064726eadaa04d423cbbe22ab7d7d0a6
SeverinuM <sewerynmichota@gmail.com>
2020-12-19 20:10:12 +0000

    OrderController impl - first commit

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,18 +1,33 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.OrderDto;
+import com.kodilla.ecommercee.exceptions.OrderNotFoundException;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.OrderDbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.http.MediaType;
+
 
 @RestController
 @RequestMapping("/v1/order")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderMapper orderMapper;
+    private final OrderDbService orderDbService;
+
+    public OrderController() {
+    }
+
+
     @GetMapping(value = "getOrder/{id}")
-    public OrderDto getOrder(@PathVariable Long id ) {
-        return new OrderDto(1L,10.99);
+    public OrderDto getOrder(@PathVariable Long id ) throws OrderNotFoundException {
+        return orderMapper.mapToOrderDto(
+          orderDbService.getOrder(id).orElseThrow(OrderNotFoundException::new)
+        );
     }
 
     @GetMapping(value = "getOrders")
--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -1,4 +1,11 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.dto.OrderDto;
+
 public class OrderMapper {
+
+    public OrderDto mapToOrderDto(final Order order){
+        return new OrderDto();
+    }
 }
