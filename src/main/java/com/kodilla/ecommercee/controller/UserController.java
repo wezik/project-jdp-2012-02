1da6061e9f2c3dc12ab637ccd1b3a9a492dd38ec
tosuch <tomasz.osuch@gmail.com>
2020-12-22 00:27:12 +0100

    task-JDP201202-25 rzeczywista implementacja UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,37 +1,45 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.User;
 import com.kodilla.ecommercee.dto.UserDto;
+import com.kodilla.ecommercee.exceptions.UserNotFoundException;
+import com.kodilla.ecommercee.mapper.UserMapper;
+import com.kodilla.ecommercee.service.UserDbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserDbService userService;
+    private final UserMapper userMapper;
 
     @GetMapping("/getUser/{id}")
     public UserDto getUser(@PathVariable Long id) {
-        return new UserDto(id, "test", "1", 1234L);
+        return userMapper.mapToUserDto(userService.getUserById(id).orElseThrow(UserNotFoundException::new));
     }
 
     @GetMapping("/getUsers")
     public List<UserDto> getUsers() {
-        return Arrays.asList(
-                new UserDto(1L, "test1", "1", 1234L),
-                new UserDto(2L, "test2", "1", 2234L),
-                new UserDto(3L, "test", "0", 3234L)
-        );
+        List<User> users = userService.getAllUsers();
+        return userMapper.mapToUserDtoList(users);
     }
 
     @PutMapping(value = "updateUser")
     public UserDto updateUser(@RequestBody UserDto userDto) {
-        return userDto;
+        User user = userMapper.mapToUser(userDto);
+        User saveUSer = userService.saveUser(user);
+        return userMapper.mapToUserDto(saveUSer);
     }
 
     @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDto createUser(@RequestBody UserDto userDto) {
-        return userDto;
+    public void createUser(@RequestBody UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
+        userService.saveUser(user);
     }
 }
